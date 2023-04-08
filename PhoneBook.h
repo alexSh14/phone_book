f6cb649b0ff49714ac796670b02271fbbf4f1eaa
elena <haleeva-elena@mail.ru>
2023-04-08 13:26:01 +0300

    Update PhoneBook.h

--- PhoneBook.h
+++ PhoneBook.h
@@ -6,25 +6,36 @@
 using namespace std;
 
 void Insert(map<string, string>& phoneBook, string phone, string name) {
-    //TODO реализовать функцию
+    phoneBook.insert(pair<string, string>(phone, name));
 }
 
 void UpdateName(map<string, string>& phoneBook, string phone, string name) {
-    //TODO реализовать функцию
+    //TODO Добавить проверку на существование телефона (phone), перед обновлением имени
+    // Если не существует - то просто игнорируем.
+    phoneBook[phone] = name;
 }
 
 void UpdatePhone(map<string, string>& phoneBook, string phone, string name) {
     //TODO реализовать функцию
 }
 
-void Delete(map<string, string>& phoneBook, string phone, string name) {
-    //TODO реализовать функцию
+void Delete(map<string, string>& phoneBook, string phone) {
+    phoneBook.erase(phone);
 }
 
 string FindByPhone(map<string, string>& phoneBook, string phone) {
-    //TODO реализовать функцию
+    //TODO Продумать что возвращать в случае отсутствия номера в массиве
+    return phoneBook[phone];
 }
 
 map<string, string> FindByName(map<string, string>& phoneBook, string name) {
-    //TODO реализовать функцию
+    map<string, string> result;
+
+    for (auto item : phoneBook) {
+        if (item.second == name) {
+            result.insert(item);
+        }
+    }
+
+    return result;
 }
--- main.cpp
+++ main.cpp
@@ -5,6 +5,25 @@
 using namespace std;
 
 int main() {
+    auto phoneBook = ImportFromFile("export.psv");
+    ShowAll(phoneBook);
+    cout << " --- " << endl;
+
+    Insert(phoneBook, "D", "DD");
+    ShowAll(phoneBook);
+    cout << " --- " << endl;
+
+    UpdateName(phoneBook, "C", "CC");
+    ShowAll(phoneBook);
+    cout << " --- " << endl;
+
+    Delete(phoneBook, "B");
+    ShowAll(phoneBook);
+    cout << " --- " << endl;
+
+    auto name = FindByPhone(phoneBook, "D");
+    cout << name << endl;
+    cout << " --- " << endl;
 
     return 0;
 }
